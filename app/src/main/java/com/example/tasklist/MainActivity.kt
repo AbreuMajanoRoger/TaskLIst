8a2d2c7a4511961785d9d69c911400879d05c27f
roger <abreumajanodev@gmail.com>
2024-07-02 14:00:13 +0200

    agregado funciones de agregar, actualizar y borrar. Se añadio algunas cosas del layout para mostrar las tareas

--- app/src/main/java/com/example/tasklist/MainActivity.kt
+++ app/src/main/java/com/example/tasklist/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.tasklist
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Layout
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import com.example.tasklist.data.MydatabaseHelper
+import com.example.tasklist.databinding.ActivityMainBinding
+import org.intellij.lang.annotations.Identifier
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tareasDbHelper: MydatabaseHelper
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        tareasDbHelper = MydatabaseHelper(this,null)
+        with(binding){
+
+            btnAgregar.setOnClickListener {
+                if (editText.text.isNotBlank()){
+                    tareasDbHelper.agregarTarea(editText.text.toString()
+                    )
+
+                    editText.clearComposingText()
+                }
+                else{ Toast.makeText(this, "Tarea agregada", Toast.LENGTH_SHORT).show() }
+
+            }
+
+
+            btnActualizar.setOnClickListener {
+                if(editText.text.isNotBlank()){
+                    solicitarIdentificador(UPDATE)
+                } else{ Toast.makeText(this, "Tarea agregada", Toast.LENGTH_SHORT).show()
+                    }
+
+
+            }
+
+
+
+
+
+            fun solicitarIdentificador(accion: String){
+                val myDialogView = LayoutInflater.from(this@MainActivity).inflate(R.layout.dialogo,null)
+            }
+
+
         }
+
+
+
+
+
+
+
     }
-}
+
+
+
+    }
